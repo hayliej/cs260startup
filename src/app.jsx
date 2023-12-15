27f8e4e7fccba7c0f783ec906e5ba5c69d0a7209
Haylie Jarvis <hjballet@yahoo.com>
2023-12-15 13:16:36 -0700

    added some react files

---
+++ src/app.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { BrowserRouter, NavLink, Route, Routes } from 'react-router-dom';
+import { Login } from './login/login';
+import { Play } from './play/play';
+import { Scores } from './scores/scores';
+import { About } from './about/about';
+import { AuthState } from './login/authState';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './app.css';
