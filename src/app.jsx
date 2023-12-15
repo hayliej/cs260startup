e9fd762315b89878090281641daf9fb130d77733
Haylie Jarvis <hjballet@yahoo.com>
2023-12-15 13:31:19 -0700

    more r

--- src/app.jsx
+++ src/app.jsx
@@ -7,3 +7,7 @@ import { About } from './about/about';
 import { AuthState } from './login/authState';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './app.css';
+
+export default function App() {
+  return <div className='body bg-dark text-light'>App will display here</div>;
+}
